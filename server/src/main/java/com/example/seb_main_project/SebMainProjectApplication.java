0b732a2675c03ab4a9608c52e91cfc0b22e7921e
dev32user <dev32user@gmail.com>
2022-11-10 23:27:50 +0900

    [INIT] application.yml 파일 제외한 백엔드 프로젝트 생성
    백엔드용 브랜치로 구별된 스프링 프로젝트를 생성하였습니다.

---
+++ server/src/main/java/com/example/seb_main_project/SebMainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.seb_main_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebMainProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebMainProjectApplication.class, args);
+    }
+
+}
