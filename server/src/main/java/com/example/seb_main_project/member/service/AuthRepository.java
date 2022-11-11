8734726af396d465423d1b29a75e539bf97deff8
dev32user <dev32user@gmail.com>
2022-11-11 14:39:05 +0900

    [FEAT] create findByEmail method on AuthRepository repository interface

--- server/src/main/java/com/example/seb_main_project/member/service/AuthRepository.java
+++ server/src/main/java/com/example/seb_main_project/member/service/AuthRepository.java
@@ -3,5 +3,8 @@ package com.example.seb_main_project.member.service;
 import com.example.seb_main_project.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
