2191a41eef8561dc6414f88eeb52a1a2b7a1fc6b
skynolimit <sky221b9@gmail.com>
2022-11-18 13:54:53 +0900

    Feat:이메일비밀번호유효성검사추가

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -32,8 +32,8 @@ const LoginInput = styled.input`
 		outline: solid 0.2rem #91f841;
 	}
 `;
-const PasswordInput = styled(LoginInput)`
-	margin-bottom: 2rem;
+const LoginButton = styled(GreenBtn)`
+	margin: 1rem;
 `;
 const LogoDiv = styled.img.attrs({
 	src: 'https://user-images.githubusercontent.com/99412221/202052092-56e52c9b-0654-45e0-9591-3cf9ac047a2a.png',
@@ -65,16 +65,68 @@ const WelcomeStr = styled.div`
 	text-align: center;
 	font-weight: 700;
 `;
-
+const ErrorEmail = styled.div`
+	color: red;
+	font-size: 0.8rem;
+	margin-top: 0.5rem;
+	margin-right: 14rem;
+`;
+const ErrorPw = styled.div`
+	color: red;
+	font-size: 0.8rem;
+	margin-top: 0.5rem;
+	margin-right: 4rem;
+`;
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [pw, setPw] = useState('');
+
+	const [emailValid, setEmailValid] = useState(false);
+	const [pwValid, setPwValid] = useState(false);
+
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+		const regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+		if (regex.test(email)) {
+			setEmailValid(true);
+		} else {
+			setEmailValid(false);
+		}
+	};
+	const handlePw = (e) => {
+		setPw(e.target.value);
+		const regex = /^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,24}$/;
+		if (regex.test(pw)) {
+			setPwValid(true);
+		} else {
+			setPwValid(false);
+		}
+	};
+
 	return (
 		<div>
 			<PageContainer>
 				<LoginContainer>
 					<LogoDiv />
-					<LoginInput type="email" name="loginEmail" placeholder="Enter your email"></LoginInput>
-					<PasswordInput type="password" name="loginPassword" placeholder="Enter your password"></PasswordInput>
-					<GreenBtn text="Log in" />
+					<LoginInput
+						type="email"
+						name="loginEmail"
+						value={email}
+						onChange={handleEmail}
+						placeholder="Enter your email"></LoginInput>
+					<div>{!emailValid && email.length > 0 && <ErrorEmail>올바른 이메일을 입력해주세요</ErrorEmail>}</div>
+					<LoginInput
+						type="password"
+						name="loginPassword"
+						value={pw}
+						onChange={handlePw}
+						placeholder="Enter your password"></LoginInput>
+					<div>
+						{!pwValid && pw.length > 0 && (
+							<ErrorPw>최소 8 자, 하나 이상의 문자, 숫자 및 특수 문자를 포함합니다</ErrorPw>
+						)}
+					</div>
+					<LoginButton text="Log in" />
 					<div>
 						Don’t have an account? <Link to="/signup">Sign up</Link>
 					</div>
