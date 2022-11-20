924f4291d16108b528f7420145d7363895e7e748
dev32user <dev32user@gmail.com>
2022-11-21 07:29:32 +0900

    [FEAT] 멤버 엔티티 변경

--- server/src/main/java/com/example/seb_main_project/member/entity/Member.java
+++ server/src/main/java/com/example/seb_main_project/member/entity/Member.java
@@ -1,10 +1,12 @@
 package com.example.seb_main_project.member.entity;
 
+import com.example.seb_main_project.audit.Auditable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,7 @@ import java.util.List;
 @Setter
 @Entity
 @NoArgsConstructor
-public class Member {
+public class Member extends Auditable implements Principal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +28,9 @@ public class Member {
     // Security 유저 권한
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles = new ArrayList<>();
+
+    @Override
+    public String getName() {
+        return getEmail();
+    }
 }
