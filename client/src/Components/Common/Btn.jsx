9d5f7685cc652e68a887e4418735cc5a2daf928a
seung-yoon-yu <seung.yoon.yu@gmail.com>
2022-11-21 16:52:56 +0900

    FEAT: 회색 버튼 추가

--- client/src/Components/Common/Btn.jsx
+++ client/src/Components/Common/Btn.jsx
@@ -14,6 +14,14 @@ export const GreenBtn = ({ className, text, callback }) => {
 		</GreenBtnStyle>
 	);
 };
+export const GreyBtn = ({ className, text, callback }) => {
+	return (
+		<GreyBtnStyle onClick={callback} className={className}>
+			{text}
+		</GreyBtnStyle>
+	);
+};
+
 const BlackBtnStyle = styled.button`
 	padding: 0.5rem 1rem 0.5rem 1rem;
 	border-radius: 22px;
@@ -39,6 +47,24 @@ const GreenBtnStyle = styled.button`
 	text-align: center;
 	cursor: pointer;
 	background-color: #91f841;
+	color: #333333;
+
+	&:hover {
+		filter: brightness(90%);
+	}
+	&:active {
+		filter: brightness(80%);
+	}
+`;
+
+const GreyBtnStyle = styled.button`
+	padding: 0.5rem 1rem 0.5rem 1rem;
+	border-radius: 22px;
+	font-weight: 500;
+	font-size: 0.85rem;
+	text-align: center;
+	cursor: pointer;
+	background-color: #cccccc;
 	color: #333333;
 
 	&:hover {
