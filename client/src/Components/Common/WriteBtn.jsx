433a9e08004c116b346f5c9bc01520ca15984bb4
seung-yoon-yu <seung.yoon.yu@gmail.com>
2022-11-22 15:35:42 +0900

    FEAT: 글 작성 버튼 추가

---
+++ client/src/Components/Common/WriteBtn.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import post_button from '../../Assets/img/post_button.svg';
+
+const WriteBtn = () => {
+  return <Container />;
+};
+const Container = styled.button`
+  position: fixed;
+  bottom: 2rem;
+  right: 2rem;
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: 0 0 5px #ccc;
+  background-image: url(${post_button});
+  background-size: 1.75rem;
+  background-repeat: no-repeat;
+  background-position: 1rem;
+  cursor: pointer;
+  :hover {
+    background-color: #eee;
+  }
+  :active {
+    background-color: #ddd;
+  }
+`;
+export default WriteBtn;
