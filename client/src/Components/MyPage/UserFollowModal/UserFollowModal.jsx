22c091bc4b371d25cf44bcc2deb75a93de5d1729
roin09 <roin14@gmail.com>
2022-11-22 15:07:16 +0900

    Feat: 다른유저 Mypage Follow목록 추가

--- client/src/Components/MyPage/UserFollowModal/UserFollowModal.jsx
+++ client/src/Components/MyPage/UserFollowModal/UserFollowModal.jsx
@@ -1,18 +1,19 @@
 import { Modal, ToggleBtn, Follow, FollowContainer, TogglebtnContainer, Container, FollowBtn } from './style';
 import { getFollowing, getFollower, addFollow } from '../../../Api/MyinfoApi';
 import { useState, useEffect } from 'react';
-const FollowModal = (props) => {
+const UserFollowModal = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
   const { open, close, header, select } = props;
   const [choice, setChoice] = useState(select);
-  const [followingList, setFollowingList] = useState([]);
-  const [followerList, setFollowerList] = useState([]);
+  const [UserFollowingList, setUserFollowingList] = useState([]);
+  const [MyFollowingList, setMyFollowingList] = useState([]);
+  const [UserFollowerList, setUserFollowerList] = useState([]);
 
   const handleChoice = (e) => {
     setChoice(e.target.value);
   };
   const removeFollowing = (id) => {
-    setFollowingList(followingList.filter((user) => user.id !== id));
+    setUserFollowingList(UserFollowingList.filter((user) => user.id !== id));
     addFollow(id).then((res) => {
       console.log(res);
     });
@@ -22,29 +23,39 @@ const FollowModal = (props) => {
     let isValidScope = true;
 
     const fetchData = async () => {
-      let followingsList = await getFollowing();
+      //다른유저 following
+      let UserFollowingsList = await getFollowing();
       if (!isValidScope) {
         return;
       }
-
-      let followersList = await getFollower();
+      //다른유저 follower
+      let UserFollowersList = await getFollower();
       if (!isValidScope) {
         return;
       }
+      //내 following
+      const MyFollowingList = await getFollowing();
+      if (!isValidScope) {
+        return;
+      }
+      setMyFollowingList(MyFollowingList);
+      //내 following 유저 ID
+      const MyFollowingUserIds = MyFollowingList?.map((f) => f.id);
 
-      const followingUserIds = followingList?.map((f) => f.id);
-
-      followersList = followersList?.map((follower) => {
-        return followingUserIds?.includes(follower.id) ? { ...follower, isFollowing: true } : follower;
+      //다른유저 follower 중 내가 follow한 사람 여부
+      UserFollowersList = UserFollowersList?.map((follower) => {
+        return MyFollowingUserIds?.includes(follower.id) ? { ...follower, isFollowing: true } : follower;
       });
 
-      setFollowerList(followersList);
+      setUserFollowerList(UserFollowersList);
 
-      followingsList = followingsList?.map((following) => {
-        return followingUserIds?.includes(following.id) ? { ...following, isFollowing: true } : following;
+      //다른유저 following 중 내가 follow한 사람 여부
+
+      UserFollowingsList = UserFollowingsList?.map((following) => {
+        return MyFollowingUserIds?.includes(following.id) ? { ...following, isFollowing: true } : following;
       });
 
-      setFollowingList(followingsList);
+      setUserFollowingList(UserFollowingsList);
     };
 
     fetchData();
@@ -53,33 +64,35 @@ const FollowModal = (props) => {
       isValidScope = false;
     };
   }, []);
+  //다른유저 follower 버튼
   const onFollowFollower = (followerId) => {
-    const followersList = followerList?.map((follower) => {
+    const UserFollowersList = UserFollowerList?.map((follower) => {
       return follower.id === followerId ? { ...follower, isFollowing: true } : follower;
     });
-    setFollowerList(followersList);
+    setUserFollowerList(UserFollowersList);
   };
 
   const onUnfollowFollower = (followerId) => {
-    const followersList = followerList?.map((follower) => {
+    const UserFollowersList = UserFollowerList?.map((follower) => {
       return follower.id === followerId ? { ...follower, isFollowing: false } : follower;
     });
-    setFollowerList(followersList);
+    setUserFollowerList(UserFollowersList);
   };
-
+  // 다른유저 following 버튼
   const onFollowFollowing = (followingId) => {
-    const followingsList = followingList?.map((following) => {
+    const UserFollowingsList = UserFollowingList?.map((following) => {
       return following.id === followingId ? { ...following, isFollowing: true } : following;
     });
-    setFollowingList(followingsList);
+    setUserFollowingList(UserFollowingsList);
   };
 
   const onUnfollowFollowing = (followingId) => {
-    const followingsList = followingList?.map((following) => {
+    const UserFollowingsList = UserFollowingList?.map((following) => {
       return following.id === followingId ? { ...following, isFollowing: false } : following;
     });
-    setFollowingList(followingsList);
+    setUserFollowingList(UserFollowingsList);
   };
+
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
     <Modal popup={open ? 'popup' : ''} onClick={close}>
@@ -105,13 +118,14 @@ const FollowModal = (props) => {
               <FollowContainer>
                 <Follow>
                   {choice === 'following'
-                    ? followingList.map((follow, idx) => {
+                    ? UserFollowingList.map((follow, idx) => {
                         return (
                           <div className="follow-item" key={idx}>
                             <div className="follow-img">
                               <img src={follow.profileImg} alt="UserPic" />
                             </div>
                             <div className="follow-name">{follow.nickname}</div>
+
                             {follow?.isFollowing ? (
                               <FollowBtn status={'following'} onClick={() => onUnfollowFollowing(follow.id)}>
                                 Unfollow
@@ -125,7 +139,7 @@ const FollowModal = (props) => {
                           </div>
                         );
                       })
-                    : followerList.map((follow, idx) => {
+                    : UserFollowerList.map((follow, idx) => {
                         return (
                           <div className="follow-item" key={idx}>
                             <div className="follow-img">
@@ -157,4 +171,4 @@ const FollowModal = (props) => {
   );
 };
 
-export default FollowModal;
+export default UserFollowModal;
