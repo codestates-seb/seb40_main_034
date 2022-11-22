51a7117e72cff7968397cc4e0f9a5989ebddcf5d
skynolimit <sky221b9@gmail.com>
2022-11-22 16:35:33 +0900

    Feat:CookieandToken.ver1

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './initializer.css';
 import App from './App';
+import store from './Store';
+//CookiesProvider 와 Provider 선언으로 이제 Cookie와 Redux를 사용할 수 있다.
+import { Provider } from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CookiesProvider>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </CookiesProvider>,
 );
--- client/src/storage/Cookie.js
+++ client/src/storage/Cookie.js
@@ -2,6 +2,7 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
+//setRefreshToken:RefreshToken을 Cookie에 저장하기 위한 함수
 export const setRefreshToken = (refreshToken) => {
   return cookies.set('Refresh', refreshToken, {
     sameSite: 'strict',
@@ -9,11 +10,11 @@ export const setRefreshToken = (refreshToken) => {
     maxAge: 420 * 60, // 420분
   });
 };
-
+//getCookieToken : Cookie에 저장된 Refresh Token 값을 갖고 오기 위한 함수.
 export const getCookieToken = () => {
   return cookies.get('Refresh');
 };
-
+//emoveCookieToken : Cookie 삭제를 위한 함수. 로그아웃 시 사용
 export const removeCookieToken = () => {
   return cookies.remove('Refresh', { sameSite: 'strict', path: '/' });
 };
