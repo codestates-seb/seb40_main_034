3b3a1a2aa1f7060d95e4466476f5b6c617f34510
aroowsVd <noya716@naver.com>
2022-11-24 12:23:52 +0900

    전역 컨테이너

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import styled from 'styled-components';
 import { AppRouter } from './Routes/AppRouter';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Main_container>
+      <AppRouter />
+    </Main_container>
+  );
 }
+
+const Main_container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 export default App;
