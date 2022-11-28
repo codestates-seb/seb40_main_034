f20250310b0c0d460d74f81695d0af1a18644409
roin09 <roin14@gmail.com>
2022-11-28 18:03:38 +0900

    Fix: Redux Auth 파일 수정

--- client/src/Store/Auth.js
+++ client/src/Store/Auth.js
@@ -10,26 +10,29 @@ export const tokenSlice = createSlice({
     tags: [],
   },
   reducers: {
-    SET_LoginUserInfo: (state, action) => {
-      state.authenticated = true;
-      state.memberId = action.payload.memberId;
-      state.email = action.payload.email;
-      state.nickname = action.payload.nickname;
-    },
-    DELETE_LoginUserInfo: (state) => {
-      state.authenticated = false;
-      state.memberId = null;
-      state.email = null;
-      state.nickname = null;
-    },
-    SET_TagsInfo: (state, action) => {
+    setLoginUserInfo: (state, action) => ({
+      ...state,
+      authenticated: true,
+      memberId: action.payload.memberId,
+      email: action.payload.email,
+      nickname: action.payload.nickname,
+    }),
+    deleteLoginUserInfo: (state) => ({
+      ...state,
+      authenticated: false,
+      memberId: null,
+      email: null,
+      nickname: null,
+    }),
+    setTagsInfo: (state, action) => ({
       // 추후에 수정
-      state.tags = action.payload.tags;
-    },
+      ...state,
+      tags: action.payload.tags,
+    }),
   },
 });
 //SET_TOKEN : Access Token 정보를 저장한다.
 //DELETE_TOKEN : 값을 모두 초기화함으로써 Access Token에 대한 정보도 삭제한다.
-export const { SET_LoginUserInfo, DELETE_LoginUserInfo, SET_TagsInfo } = tokenSlice.actions;
+export const { setLoginUserInfo, deleteLoginUserInfo, setTagsInfo } = tokenSlice.actions;
 
 export default tokenSlice.reducer;
