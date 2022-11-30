2aff7603fd0319175051213ae8e016a229d07c37
Seungyoon Yu <seung.yoon.yu@gmail.com>
2022-11-30 14:22:44 +0900

    FEAT: 탭 링크 연결

--- client/src/Components/Common/Tab.jsx
+++ client/src/Components/Common/Tab.jsx
@@ -3,24 +3,59 @@ import HomeIcon from '../../Assets/img/tab_home.svg';
 import FollowedIcon from '../../Assets/img/tab_followed.svg';
 import MapIcon from '../../Assets/img/tab_map.svg';
 import BookmarkIcon from '../../Assets/img/tab_bookmark.svg';
+import { Link } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
 
 const Tab = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const modal = useRef(null);
+  const profile = useRef(null);
+  const handleClick = () => {
+    clicked === false ? setClicked(true) : setClicked(false);
+  };
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (profile.current.contains(e.target)) return;
+      if (modal.current && !modal.current.contains(e.target)) setClicked(false);
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [modal]);
+
   return (
     <Container>
       <CategoryLeft>
-        <div />
+        <Link to="/">
+          <div className="tabMenu" />
+        </Link>
       </CategoryLeft>
       <CategoryMid>
-        <div />
+        <div className="tabMenu" />
       </CategoryMid>
       <CategoryMid2>
-        <div />
+        <Link to="/map">
+          <div className="tabMenu" />
+        </Link>
       </CategoryMid2>
       <CategoryRight>
-        <div />
+        <Link to="/">
+          <div className="tabMenu" />
+        </Link>
       </CategoryRight>
       <HeaderProfile>
-        <Profile />
+        {/*로그인되어 있으면 프로필에서 모달 메뉴 나오고, 그렇지 않으면 로그인 회원가입 버튼 보여야 함*/}
+        <Profile onClick={handleClick} ref={profile} />
+        <Modal clicked={clicked} ref={modal}>
+          <div className="link">
+            <Link to="/">Profile</Link>
+          </div>
+          <div className="link">
+            <Link to="/logout">Sign Out</Link>
+          </div>
+        </Modal>
       </HeaderProfile>
     </Container>
   );
@@ -33,7 +68,7 @@ const Container = styled.div`
 
 const CategoryLeft = styled.div`
   width: 20%;
-  div {
+  .tabMenu {
     margin-left: calc(50% - 1rem);
     border-radius: 20px;
     width: 2.5rem;
@@ -53,7 +88,7 @@ const CategoryLeft = styled.div`
 
 const CategoryMid = styled.div`
   width: 20%;
-  div {
+  .tabMenu {
     margin-left: calc(50% - 1rem);
     border-radius: 20px;
     width: 2.5rem;
@@ -73,7 +108,7 @@ const CategoryMid = styled.div`
 
 const CategoryMid2 = styled.div`
   width: 20%;
-  div {
+  .tabMenu {
     margin-left: calc(50% - 1rem);
     border-radius: 20px;
     width: 2.5rem;
@@ -93,7 +128,7 @@ const CategoryMid2 = styled.div`
 
 const CategoryRight = styled.div`
   width: 20%;
-  div {
+  .tabMenu {
     margin-left: calc(50% - 1rem);
     border-radius: 20px;
     width: 2.5rem;
@@ -117,9 +152,6 @@ const HeaderProfile = styled.div`
   display: flex;
   align-items: center;
   cursor: pointer;
-  & :hover {
-    filter: brightness(90%);
-  }
 `;
 
 const Profile = styled.div`
@@ -128,6 +160,38 @@ const Profile = styled.div`
   border-radius: 20px;
   background-image: ${(props) => (props.profileImg === undefined ? 'none' : props.profileImg)};
   border: ${(props) => (props.profileImg === undefined ? '1px solid #eeeeee' : 'none')};
+  &:hover {
+    filter: brightness(90%);
+  }
+`;
+const Modal = styled.div`
+  display: ${(props) => (props.clicked === true ? 'block' : 'none')};
+  border: 1px solid #ccc;
+  border-radius: 1rem;
+  width: 8rem;
+  height: auto;
+  position: absolute;
+  text-align: center;
+  top: 3rem;
+  right: 0.5rem;
+  .link {
+    cursor: pointer;
+    padding: 0.33rem;
+  }
+  .link a {
+    font-size: 0.85rem;
+  }
+  .link:first-child {
+    border-top-left-radius: 16px;
+    border-top-right-radius: 16px;
+  }
+  .link:last-child {
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 16px;
+  }
+  .link:hover {
+    background-color: #eee;
+  }
 `;
 
 export default Tab;
