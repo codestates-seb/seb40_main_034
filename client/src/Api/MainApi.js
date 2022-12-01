7e0992fe3011bf68064e530f88132bf0ea5f221c
seung-yoon-yu <seung.yoon.yu@gmail.com>
2022-12-01 14:04:50 +0900

    FEAT: 무한스크롤 기본 골격 작성

--- client/src/Api/MainApi.js
+++ client/src/Api/MainApi.js
@@ -1,11 +1,15 @@
 import instance from './root';
 
-export const getAllLists = async () => {
+export const getAllLists = async ({ page, size }) => {
   const url = '/';
   try {
-    const result = await instance.get(url);
+    const result = await instance.get(url, {
+      params: { page, size },
+    });
+    console.log('main post list data: ', result.data);
     return result.data;
   } catch (err) {
+    console.log('main post list fetch error: ', err);
     return [err];
   }
 };
--- client/src/Components/Main/ListContainer.jsx
+++ client/src/Components/Main/ListContainer.jsx
@@ -1,20 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { getAllLists } from '../../Api/MainApi';
 import { List } from '../List/List';
+
 const ListContainer = () => {
   const [postList, setPostList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [isFetching, setFetching] = useState(false);
+  const [hasNextPage, setNextPage] = useState(true);
+
+  const fetchList = useCallback(async () => {
+    const { data } = await getAllLists({ page, size: 10 });
+    setPostList(postList.concat(data.data));
+    setPage(data.pageInfo.page + 1);
+    setNextPage(!(data.pageInfo.page === data.pageInfo.totalPages));
+    setFetching(false);
+    console.log('fetched');
+  });
+
   useEffect(() => {
-    getAllLists().then((res) => {
-      setPostList(res.data);
-    });
+    const handleScroll = () => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      if (window.innerHeight + scrollTop >= offsetHeight) {
+        setFetching(true);
+      }
+      console.log('scrolled');
+    };
+    setFetching(true);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+
+  useEffect(() => {
+    if (isFetching && hasNextPage) fetchList();
+    else if (!hasNextPage) setFetching(false);
+  }, [isFetching]);
   return (
     <Container>
       {postList &&
         postList.map((post) => {
           <List key={post.postId} nickname={post.nickname} postId={post.postId} createdAt={post.createdAt} />;
         })}
+      {isFetching && <Loading>Loading...</Loading>}
     </Container>
   );
 };
@@ -25,4 +52,5 @@ const Container = styled.div`
   justify-content: center;
 `;
 
+const Loading = styled.div``;
 export default ListContainer;
